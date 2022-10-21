14902b009dc9fc37b3b5b91e3e2317b379a0f0da
Gordana Marjanovic <gogamar@gmail.com>
2022-10-21 11:16:04 +0200

    changes

--- db/migrate/20221020193823_set_user_column_not_null_vrentaltemplates.rb
+++
@@ -1,5 +0,0 @@
-class SetUserColumnNotNullVrentaltemplates < ActiveRecord::Migration[7.0]
-  def change
-    change_column_null :vrentaltemplates, :user_id, false
-  end
-end
--- db/migrate/20221021090240_set_user_column_null_vrentaltemplates.rb
+++
@@ -1,5 +0,0 @@
-class SetUserColumnNullVrentaltemplates < ActiveRecord::Migration[7.0]
-  def change
-    change_column_null :vrentaltemplates, :user_id, true
-  end
-end
