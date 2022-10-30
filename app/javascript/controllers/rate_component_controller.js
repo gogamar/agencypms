5409c4f0afa9918063e97af70d2e48c5584514c8
Gordana Marjanovic <gogamar@gmail.com>
2022-10-30 11:11:36 +0100

    changed stimulus rate controller

--- app/javascript/controllers/rate_component_controller.js
+++ app/javascript/controllers/rate_component_controller.js
@@ -5,13 +5,13 @@ export default class extends Controller {
   static targets = ["checkin", "checkout"];
 
   connect() {
-    console.log("Rate component controller connected!");
+    console.log("Rate component controller connected default date activated!");
     const unvailableDates = JSON.parse(this.element.dataset.unavailable);
 
     const checkinPicker = this.checkinTarget.flatpickr({
       altInput: true,
       altFormat: "D, d/m/y",
-      defaultDate: this.element.dataset.defaultCheckinDate,
+      // defaultDate: this.element.dataset.defaultCheckinDate,
       disable: unvailableDates,
       minDate: "today",
       disableMobile: true,
@@ -23,7 +23,7 @@ export default class extends Controller {
     const checkoutPicker = this.checkoutTarget.flatpickr({
       altInput: true,
       altFormat: "D, d/m/y",
-      defaultDate: this.element.dataset.defaultCheckoutDate,
+      // defaultDate: this.element.dataset.defaultCheckoutDate, // when activated, on change doesn't change the month on checkout
       disable: unvailableDates,
       minDate: "today",
       disableMobile: true,
