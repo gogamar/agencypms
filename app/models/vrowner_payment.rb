d63fe268af0d0f0257285f3d06440562aa1e8534
Gordana Marjanovic <gogamar@gmail.com>
2023-09-19 17:13:59 +0200

    h

--- app/models/vrowner_payment.rb
+++ app/models/vrowner_payment.rb
@@ -1,6 +1,17 @@
 class VrownerPayment < ApplicationRecord
   belongs_to :statement
   validates :statement_id, uniqueness: true
+  validates :amount, :date, presence: true
+  validate :amount_cannot_be_zero
+
 
   PAYMENT_METHODS = %w(transfer check cash).freeze
+
+  private
+
+  def amount_cannot_be_zero
+    if amount.present? && amount.zero?
+      errors.add(:amount, "no pot ser zero")
+    end
+  end
 end
