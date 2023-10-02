aa5855555ad2e4d3f40ca667813fd88e5d483e36
Gordana Marjanovic <gogamar@gmail.com>
2023-10-02 12:31:05 +0200

    fix bug index expenses

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,6 +1,8 @@
 class Expense < ApplicationRecord
   belongs_to :vrental, optional: false
   belongs_to :statement, optional: true
+  validates :amount, presence: true
+  validates :expense_type, presence: true, inclusion: { in: %w(agency owner) }
 
   EXPENSE_TYPES = %w(agency owner).freeze
 end
