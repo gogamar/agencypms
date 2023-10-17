1430a066ac136ca2e511d059c950d4fa4b2b1fc7
Gordana Marjanovic <gogamar@gmail.com>
2023-10-17 12:10:44 +0200

    comment out js packs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,12 +4,12 @@ import "./controllers";
 import "bootstrap";
 import "./add_jquery";
 import "./packs/custom";
-import "./packs/imagesloaded";
-import "./packs/jquery.magnific-popup.min";
-import "./packs/lightbox";
-import "./packs/rangeslider";
-import "./packs/slick";
-import "./packs/slider-bg.min";
+// import "./packs/imagesloaded";
+// import "./packs/jquery.magnific-popup.min";
+// import "./packs/lightbox";
+// import "./packs/rangeslider";
+// import "./packs/slick";
+// import "./packs/slider-bg.min";
 import { initSelect2 } from "./components/init_select2";
 import { initFlatpickr } from "./plugins/flatpickr";
 
