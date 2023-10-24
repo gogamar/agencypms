4cae69674ac3f167cdffc033db7dc7947ba78f87
Gordana Marjanovic <gogamar@gmail.com>
2023-10-24 23:44:42 +0200

    fix naming bug

--- db/migrate/20231019092806_add_role_to_users_rename_vrowner.rb
+++ db/migrate/20231019092806_add_role_to_users_rename_vrowner.rb
@@ -1,4 +1,4 @@
-class AddRoleToUsersRenameOwner < ActiveRecord::Migration[7.0]
+class AddRoleToUsersRenameVrowner < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :role, :integer, default: 0
     User.where(admin: true).update_all(role: 2)
