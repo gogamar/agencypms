bc809a29bf23e6e620b25c70696d2425d98d3826
Gordana Marjanovic <gogamar@gmail.com>
2023-11-23 16:47:56 +0100

    validation

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -2,7 +2,7 @@ class Availability < ApplicationRecord
   belongs_to :vrental
   validates :date, presence: true
   validates :inventory, presence: true
-  validates :multiplier, numericality: { greater_than_or_equal_to: 0 }
+  # validates :multiplier, numericality: { greater_than_or_equal_to: 0 }
 
   OVERRIDE = { 0 => "none", 1 => "blackout", 2 => "no_checkin", 3 => "no_checkout", 4 => "no_checkin_out" }.freeze
 end
