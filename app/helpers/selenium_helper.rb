4b46bcb1ff3ce406dba7b0e57f94687b5a1f1f91
Gordana Marjanovic <gogamar@gmail.com>
2023-12-21 12:30:54 +0100

    selenium on heroku

--- app/helpers/selenium_helper.rb
+++ app/helpers/selenium_helper.rb
@@ -2,10 +2,25 @@
 
 require 'selenium-webdriver'
 
+# for local development
+
+# module SeleniumHelper
+#   def setup_selenium_driver
+#     driver_path = '/usr/local/bin/chromedriver'
+#     service = Selenium::WebDriver::Chrome::Service.new(path: driver_path)
+#     options = Selenium::WebDriver::Chrome::Options.new
+#     options.add_argument('--headless')
+#     driver = Selenium::WebDriver.for :chrome, options: options, service: service
+#     return driver
+#   end
+# end
+
+# for Heroku deployment
+
 module SeleniumHelper
   def setup_selenium_driver
-    driver_path = '/usr/local/bin/chromedriver'
-    service = Selenium::WebDriver::Chrome::Service.new(path: driver_path)
+    # No need to specify the driver path on Heroku
+    service = Selenium::WebDriver::Chrome::Service.new
     options = Selenium::WebDriver::Chrome::Options.new
     options.add_argument('--headless')
     driver = Selenium::WebDriver.for :chrome, options: options, service: service
