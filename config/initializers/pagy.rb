feac5fd2fcb2881684c174106de56c5709053604
Gordana Marjanovic <gogamar@gmail.com>
2024-01-26 10:01:01 +0100

    email

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -20,7 +20,7 @@
 
 # Other Variables
 # See https://ddnexus.github.io/pagy/docs/api/pagy#other-variables
-# Pagy::DEFAULT[:size]       = [1,4,4,1]                       # default
+Pagy::DEFAULT[:size]       = [1,2,2,1]                     # default is [1,4,4,1]
 # Pagy::DEFAULT[:page_param] = :page                           # default
 # The :params can be also set as a lambda e.g ->(params){ params.exclude('useless').merge!('custom' => 'useful') }
 # Pagy::DEFAULT[:params]     = {}                              # default
