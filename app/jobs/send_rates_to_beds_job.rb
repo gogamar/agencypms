9232399fe4807c5580aa1d1d7cf6cce0514af1e5
Gordana Marjanovic <gogamar@gmail.com>
2024-02-27 13:00:28 +0100

    get availability

--- app/jobs/send_rates_to_beds_job.rb
+++ app/jobs/send_rates_to_beds_job.rb
@@ -5,6 +5,6 @@ class SendRatesToBedsJob < ApplicationJob
     vrental = Vrental.find(vrental_id)
     VrentalApiService.new(vrental).send_rates_to_beds
     VrentalApiService.new(vrental).update_min_stay_on_beds
-    VrentalApiService.new(vrental).send_availabilities_to_beds_24
+    VrentalApiService.new(vrental).get_availabilities_from_beds_24
   end
 end
