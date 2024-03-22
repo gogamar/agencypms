7589533f59f23e91c6510827ac57041db1d3892b
Gordana Marjanovic <gogamar@gmail.com>
2024-03-22 23:05:49 +0100

    x

--- db/seeds.rb
+++ db/seeds.rb
@@ -323,7 +323,7 @@ barcelona_rate_group = Vrgroup.where("name ILIKE ?", "%gaud%")
 
 estartit_vrentals.each do |vrental|
   vrental.update(min_advance: 48)
-  if vrental.future_rates.present?
+  if vrental.status == "active" && vrental.prop_key.present? && vrental.future_rates.exists?
     vrental.rates.each do |rate|
       rate.update(min_advance: 2)
     end
